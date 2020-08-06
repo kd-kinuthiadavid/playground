bc2c0da9280c74e01e8b5de59eca1e70d0bacf20
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2020-08-06 17:20:55 +0300

    Updates

---
+++ precache-manifest.8496022e8b6c7f7b778b4eac93b14d49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a7dbc587274b8d4ec3e5c7931be142",
+    "url": "/kd-kinuthiadavid/playground.git/index.html"
+  },
+  {
+    "revision": "96af1a71ce30ac5aa317",
+    "url": "/kd-kinuthiadavid/playground.git/static/css/main.a8985c54.chunk.css"
+  },
+  {
+    "revision": "d3ac199a71eee555148b",
+    "url": "/kd-kinuthiadavid/playground.git/static/js/2.4a4b9da7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kd-kinuthiadavid/playground.git/static/js/2.4a4b9da7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96af1a71ce30ac5aa317",
+    "url": "/kd-kinuthiadavid/playground.git/static/js/main.1eedd683.chunk.js"
+  },
+  {
+    "revision": "c1fdb872519629560adb",
+    "url": "/kd-kinuthiadavid/playground.git/static/js/runtime-main.fc440b94.js"
+  }
+]);
